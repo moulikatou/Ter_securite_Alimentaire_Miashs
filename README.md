f310023da1bd1a5117af6ade5172ed32a0a4e9df
vmelancon <victor.melancon0@gmail.com>
2020-11-08 18:14:22 +0100

    skeleton of website done, reste a savoir ce que l'on met dedans

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Ter_securite_Alimentaire_Miashs
+
+# Quick launch
+Tout d'abord il vous faut installer python ainsi que flask sur votre ordinateur.
+ - Python : https://www.python.org/downloads/
+ - Flask : (pip install flask) https://flask.palletsprojects.com/en/1.1.x/
+
+Pour tester le serveur, il vous suffit simplement de se placer dans le directoire avec le code.
+Ensuite taper cette commande :
+
+    $ FLASK_APP=main.py flask run
+
+Maintenant le serveur tourne, il vous suffit d'aller a l'adresse suivante : 127.0.0.1:5000/
+
+# Presentation
---
+++ static/js/pdf_viewer.js
@@ -0,0 +1,59 @@
+function comeon(some) {
+    console.log(some);
+    return "yup";
+}
+
+function createView() {
+    /*
+        * cree le lecteur de pdf
+     */
+    this.div = d3.select("#maincontent").append("div")
+        .attr("id", "view");
+    
+    var height = window.innerHeight;
+    // height -= d3.select("#top-menu").node().getBoundingClientRect().height;
+    // height -= d3.select("#title").node().getBoundingClientRect().height;
+    this.viewer = this.div.append("object")
+        .attr("id", "PDFObject")
+        .attr("type", "application/pdf")
+        .attr("width", "100%")
+        .attr("height", height);
+}
+
+function displayPDF(filename, title) {
+    /*
+        * charge le pdf passer en argument, en plus de changer le titre afficher sur la page
+     */
+    // console.log(filename, title);
+    d3.select("#PDFObject").attr("data", filename);
+    d3.select("#title").text(title);
+}
+
+function createSelector(path, files, titles) {
+    // rajoute la div contenant les different input submit
+    this.div = d3.select("#maincontent").append("div")
+        .attr("id", "select");
+    // this.div = document.getElementById("maincontent").append("div");
+    // this.div.setAttribute("id", "select");
+
+    // la table pour organiser les boutons
+    this.table = this.div.append("table");
+    for (var i = 0; i < files.length; i++) {
+        var filename = files[i], title = titles[i];
+
+        // genere chaque bouton, filename=<le nom du fichier a charger>
+        // title=<le titre du fichier a afficher>
+        var tr = this.table.append("tr")
+        tr.append("label")
+            .attr("for", filename)
+        .append("input")
+            .attr("id", "button-v")
+            .attr("name", filename)
+            .attr("type", "submit")
+            .attr("value", title)
+            .on("click", function() {
+                displayPDF(path + '/' + this.getAttribute("name"), this.getAttribute("value"));
+            });
+    }
+}
+
